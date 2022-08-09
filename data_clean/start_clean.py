161a026cb1a9673ab12221f21b5e1ad1151014e6
colin <colin-bao@github.com>
2022-08-09 19:31:09 +0800

    feat：new

--- data_clean/start_clean.py
+++ data_clean/start_clean.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     # GetPriceData(data_base='tushare_daily').down_all_kline('zyyx', 'rpt_e_a', 'stockcode')
     label = GetLabelData('zyyx', 'rpt_e_a', ['stockcode', 'ann_date', 'title', 'report_type',
                                              'report_year', 'organ_name', 'author'], [-1, 0, 1, 5])
-    # label.get_csv()
+    # label.get_tag()
+    label.get_csv()
