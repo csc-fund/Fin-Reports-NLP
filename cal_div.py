9446c2dd8530c67d12b9bcc3475a8048936074c1
colin <colin-bao@github.com>
2022-08-10 15:28:23 +0800

    feat:提交新功能

--- cal_div.py
+++ cal_div.py
@@ -4,6 +4,7 @@
 # @Time      :2022/8/10 13:22
 # @Author    :Colin
 # @Note      :None
+import time
 
 from tools.data_tool import GetPriceData
 import pandas as pd
@@ -19,6 +20,7 @@ class DivData(GetPriceData):
         self.DIV_DATA = pd.DataFrame()  # 股利数据
         self.PRICE_DATA = pd.DataFrame()  # 市值数据
 
+    # 计算股息率
     def get_div_data(self):
         # ----------------获取股利数据----------------#
         self.DIV_DATA = self.SqlObj.select_table('茅台股息', ['税前每股派息', '基准股本', '除权除息日', '分红年度'])
@@ -28,11 +30,29 @@ class DivData(GetPriceData):
         self.PRICE_DATA = self.SqlObj.select_table('600519.SH_daily_basic', ['trade_date', 'circ_mv', ])
         self.PRICE_DATA['circ_mv'] = self.PRICE_DATA['circ_mv'].apply(lambda x: x * 10000)
 
-        # ----------------日度TTM---------------#
+        # --------------------------------LYR计算-------------------------------#
+        # 计算的函数
+        def find_div_lyr(df_x):
+            # 股票每日基本信息表
+            end_date = df_x['trade_date']
+            mv = df_x['circ_mv']
+
+            # 股票股利表
+            # 找出end_date前发生的最近除息除权日
+            df_div = self.DIV_DATA[
+                (self.DIV_DATA['除权除息日'] < end_date)]
+            df_div = df_div.iloc[-1:]
+            # 指标计算
+            div_count = df_div['税前现金股利'].count()
+            divrate_ttm = df_div['税前现金股利'].sum() / mv
+
+            return divrate_ttm, div_count
+
+        # --------------------------------TTM计算-------------------------------#
+        # 过去1年的日期作为计算基准
         self.PRICE_DATA['trade_date_y-1'] = self.PRICE_DATA['trade_date'].apply(lambda x: x - relativedelta(years=1))
 
-        # ----------------TTM计算---------------#
-
+        # 计算的函数
         def find_div_ttm(df_x):
             # 股票每日基本信息表
             start_date = df_x['trade_date_y-1']
@@ -51,17 +71,16 @@ class DivData(GetPriceData):
 
             return divrate_ttm, div_count
 
-        # ----------------TTM计算---------------#
-        self.PRICE_DATA[['divrate_ttm', 'div_count']] = self.PRICE_DATA.apply(
+        # ----------------股息率计算---------------#
+        # LYR
+        self.PRICE_DATA[['divrate_lyr', 'lyr_count']] = self.PRICE_DATA.apply(
+            lambda x: find_div_lyr(x), axis=1, result_type='expand')
+        # TTM
+        self.PRICE_DATA[['divrate_ttm', 'ttm_count']] = self.PRICE_DATA.apply(
             lambda x: find_div_ttm(x), axis=1, result_type='expand')
 
+        # ----------------保存---------------#
         self.PRICE_DATA.to_csv('茅台股息.csv')
-        # ----------------合并----------------#
-        # 通过左连接映射traget_column
-        # self.OUTPUT_TABLE = pd.merge(self.PRICE_DATA, self.DIV_DATA,
-        #                              how='left', on=[self.DATE_COLUMN])
 
 
-obj = DivData('zyyx')
-obj.get_div_data()
-# print(obj.PRICE_DATA)
+DivData('zyyx').get_div_data()
