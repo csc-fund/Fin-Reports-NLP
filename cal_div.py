7961263c69b671b7fb4f351a6c8c59158dfeeed2
colin <colin-bao@github.com>
2022-08-10 13:23:03 +0800

    feat:提交新功能

---
+++ cal_div.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :cal_div.py
+# @Time      :2022/8/10 13:22
+# @Author    :Colin
+# @Note      :None
+
+
+if __name__ == "__main__":
+    run_code = 0
