01cbb887eff005fd4be4c649aa988d0e6bce0c7a
colin <colin-bao@github.com>
2022-08-12 16:13:45 +0800

    feat:股息计算

--- cal_div_new.py
+++ cal_div_new.py
@@ -31,7 +31,9 @@ class CalDiv:
         self.MV_TABLE = self.MV_TABLE[['stockcode', 'ann_date', 's_val_mv']]
 
         # ----------------参数区----------------#
-        self.LAG_PERIOD = [1, 2, 3]  # 滞后2期
+        self.LAG_PERIOD = [1, 2, 3, 4]  # 滞后期
+        self.MERGE_COLUMN = ['report_year', 'dvd_pre_tax_sum', 'ann_date_max']  # 需要合并的列名
+        self.RENAME_COLUMN = {i: i + '_l{}' for i in self.MERGE_COLUMN}
 
     # 生成年度股息表
     def get_div_by_year(self):
@@ -62,16 +64,10 @@ class CalDiv:
 
             # ----------------获取历史股息信息----------------#
             for lag_t in self.LAG_PERIOD:  # 最近2次年报的股息
-                lag = pd.merge(df_date[['report_year', 'dvd_pre_tax_sum', 'dvd_pre_tax_count', 'ann_date_max']],
+                lag = pd.merge(df_date[self.MERGE_COLUMN],
                                pd.DataFrame(df_date['report_year'] - lag_t),
                                how='right', on=['report_year'], )
-
-                lag.rename(columns={'report_year': 'report_year_l{}'.format(lag_t),
-                                    'dvd_pre_tax_sum': 'dvd_pre_tax_sum_l{}'.format(lag_t),
-                                    'dvd_pre_tax_count': 'dvd_pre_tax_count_l{}'.format(lag_t),
-                                    'ann_date_max': 'ann_date_max_l{}'.format(lag_t)
-                                    }, inplace=True)
-
+                lag.rename(columns={k: str(v).format(lag_t) for k, v in self.RENAME_COLUMN.items()}, inplace=True)
                 df_date = pd.concat([df_date, lag], axis=1)
 
             # df_date_lag['va'] = df_date[df_date['report_year'] == df_date_lag['report_year']]['dvd_pre_tax_sum']
@@ -99,127 +95,129 @@ class CalDiv:
 
     # 计算股息率
 
+    def get_div_rate(self):
+        # ----------------生成id用于匹配历史股息----------------#
+        tqdm.pandas(desc='Start get_id')
 
-def get_div_rate(self):
-    # ----------------生成id用于匹配历史股息----------------#
-    tqdm.pandas(desc='Start get_id')
+        # 根据ann_date的前4位的年份生成id
+        self.MV_TABLE['id'] = self.MV_TABLE[['ann_date', 'stockcode']].progress_apply(
+            lambda x: x['stockcode'] + str(int(str(x['ann_date'])[:-4]) - 1), axis=1)
 
-    # 根据ann_date的前4位的年份生成id
-    self.MV_TABLE['id'] = self.MV_TABLE[['ann_date', 'stockcode']].progress_apply(
-        lambda x: x['stockcode'] + str(int(str(x['ann_date'])[:-4]) - 1), axis=1)
+        # ----------------合并市值表和股息表 计算股息率----------------#
+        self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['id'])
 
-    # ----------------合并市值表和股息表 计算股息率----------------#
-    self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['id'])
+        # 最近3次年报的股息
+        tqdm.pandas(desc='Start get_div_rate')
+        history_div = ['div_rate_l{}'.format(i) for i in self.LAG_PERIOD]
 
-    # 最近3次年报的股息
-    tqdm.pandas(desc='Start get_div_rate')
-    history_div = ['div_rate_l{}'.format(i) for i in self.LAG_PERIOD]
+        self.DIV_RATE_TABLE[history_div] = self.DIV_RATE_TABLE.progress_apply(
+            lambda x: [10000 * x['dvd_pre_tax_l{}'.format(i)] / x['s_val_mv'] for i in self.LAG_PERIOD],
+            axis=1,
+            result_type='expand')
 
-    self.DIV_RATE_TABLE[history_div] = self.DIV_RATE_TABLE.progress_apply(
-        lambda x: [10000 * x['dvd_pre_tax_l{}'.format(i)] / x['s_val_mv'] for i in self.LAG_PERIOD],
-        axis=1,
-        result_type='expand')
+        self.DIV_RATE_TABLE.to_csv('divrate.csv')
+        # self.DIV_RATE_TABLE.to_parquet('divrate.parquet')
 
-    self.DIV_RATE_TABLE.to_csv('divrate.csv')
-    # self.DIV_RATE_TABLE.to_parquet('divrate.parquet')
+    def get_exp_dr(self):
+        self.DIV_RATE_TABLE = pd.read_csv('603630.csv')
 
+        self.DIV_RATE_TABLE = self.DIV_RATE_TABLE[
+            ['stockcode', 'ann_date', 'report_year', 'dvd_pre_tax',
+             'report_year_l1', 'dvd_pre_tax_l1', ]]
 
-def get_exp_dr(self):
-    self.DIV_RATE_TABLE = pd.read_csv('603630.csv')
+        # ----------------获取股票名称----------------#
+        self.DIV_RATE_TABLE = self.DIV_RATE_TABLE[self.DIV_RATE_TABLE['stockcode'] == '603630.SH']
 
-    self.DIV_RATE_TABLE = self.DIV_RATE_TABLE[
-        ['stockcode', 'ann_date', 'report_year', 'dvd_pre_tax',
-         'report_year_l1', 'dvd_pre_tax_l1', ]]
+        # 按照时间降序
+        self.DIV_RATE_TABLE.sort_values(by='ann_date', ascending=False, inplace=True)
 
-    # ----------------获取股票名称----------------#
-    self.DIV_RATE_TABLE = self.DIV_RATE_TABLE[self.DIV_RATE_TABLE['stockcode'] == '603630.SH']
+        print(self.DIV_RATE_TABLE)
+        # self.DIV_RATE_TABLE.to_csv('603630.csv')
 
-    # 按照时间降序
-    self.DIV_RATE_TABLE.sort_values(by='ann_date', ascending=False, inplace=True)
+    # 重新算分红
+    def get_merge_table(self):
+        # 股息表
+        self.DIV_YEAR_TABLE = pd.read_csv('div_by_year.csv')
+        # 给成prelanddate
+        # 用上一年的匹配
+        self.MV_TABLE['report_year'] = (self.MV_TABLE['ann_date'].astype('str').str[:-4].astype('int')) - 1
+        self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['stockcode', 'report_year'])
+        # 增加静态计算逻辑列
+        # self.DIV_RATE_TABLE.to_csv('merge.csv', index=False)
+        self.DIV_RATE_TABLE.to_parquet('merge.parquet', index=False)
 
-    print(self.DIV_RATE_TABLE)
-    # self.DIV_RATE_TABLE.to_csv('603630.csv')
-
-
-# 重新算分红
-def get_merge_table(self):
-    # 股息表
-    self.DIV_YEAR_TABLE = pd.read_csv('div_by_year.csv')
-    # 给成prelanddate
-    # 用上一年的匹配
-    self.MV_TABLE['report_year'] = (self.MV_TABLE['ann_date'].astype('str').str[:-4].astype('int')) - 1
-    self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['stockcode', 'report_year'])
-    # 增加静态计算逻辑列
-    self.DIV_RATE_TABLE.to_csv('merge.csv', index=False)
-
-
-# 外推法计算预期
-def get_exp_div(self):
-    self.MERGE_TABLE = pd.read_csv('merge.csv')
-    # 增加静态计算逻辑列
-    # self.DIV_RATE_TABLE['last_div'] = self.DIV_RATE_TABLE.apply(lambda x: x['dvd_pre_taxsum'] if x[''] > 0 else 0)
-    print(self.MERGE_TABLE.columns)
-    #
-
-
-def get_no_history(self):
-    pd.options.mode.chained_assignment = None
-
-    # ----------------计算总股息----------------#
-    self.DIV_TABLE['dvd_pre_tax'] = self.DIV_TABLE['cash_dvd_per_sh_pre_tax'] * self.DIV_TABLE[
-        's_div_baseshare']
-    self.DIV_TABLE['report_year'] = self.DIV_TABLE['report_period'].astype('str').str[:-4]
-    # wind里面有已经实施但是ex_dt字段为空的值
-    # 不用ex_dt作为参照
-    self.DIV_TABLE['ann_date'] = self.DIV_TABLE['ann_date'].astype('int')
-    self.MV_TABLE['ann_date'] = self.MV_TABLE['ann_date'].astype('int')
-
-    # -------------------按照个股迭代------------- #
-    # 筛选出MV表中历史信息 ex_dt
-    for code in tqdm(self.MV_TABLE['stockcode'].unique()):
-        # 选取单个股票
-        df_div = self.DIV_TABLE[self.DIV_TABLE['stockcode'] == code]
-        df_code = self.MV_TABLE[self.MV_TABLE['stockcode'] == code]
-
-        # -------------------按照日期逐步迭代------------- #
-        # 按照当前日期搜索历史信息
-        def search_history(x):
-            # 按照ann_date和ex_dt筛选历史股息信息
-            div_info = df_div[df_div['ann_date'] < x['ann_date']]
-
-            # 按照年份计算筛选后历史的股息
-            div_info_year = div_info.groupby(['report_year']).agg({'dvd_pre_tax': 'sum', 'stockcode': 'count'})
-            div_info_year.reset_index(inplace=True)
-            # if not div_info_year.empty:
-            #     print(div_info_year)
-            # 滞后
-
-            div_info_year = div_info_year[div_info_year['report_year'] == str(int(str(x['ann_date'])[:-4]) - 1)]
-            # if not div_info_year.empty:
-            #     print(div_info_year)
-
-            return div_info_year['dvd_pre_tax'].values * 10000, div_info_year['stockcode'].values
-            # if div_info_year.empty:
-            #     return None, None
-            # else:
-            #     print(div_info_year['dvd_pre_tax'].loc[0], div_info_year['stockcode'].loc[0])
-            #     return div_info_year['dvd_pre_tax'].loc[0], div_info_year['stockcode'].loc[0]
-
+    # 外推法计算预期
+    def get_exp_div(self):
         #
-        df_code[['dvd_l1', 'dvd_count_l1']] = df_code.apply(lambda x: search_history(x), axis=1,
-                                                            result_type='expand')
-        self.DIV_RATE_TABLE = pd.concat([self.DIV_RATE_TABLE, df_code])
+        self.MERGE_TABLE = pd.read_parquet('merge.parquet')
+        # ----------------在截面数据中计算----------------#
+        # 静态股利
+        self.DIV_RATE_TABLE['last_div'] = self.DIV_RATE_TABLE.apply(lambda x: x['dvd_pre_taxsum'] if x[''] > 0 else 0)
+        print(self.MERGE_TABLE.columns)
 
-    self.DIV_RATE_TABLE.to_csv('history.csv')
-    # print(self.DIV_TABLE['report_year'])
+    def get_no_history(self):
+        pd.options.mode.chained_assignment = None
 
-    # 匹配
-    # self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['code', ''])
-    # 去除匹配后时间错误的列
+        # ----------------计算总股息----------------#
+        self.DIV_TABLE['dvd_pre_tax'] = self.DIV_TABLE['cash_dvd_per_sh_pre_tax'] * self.DIV_TABLE[
+            's_div_baseshare']
+        self.DIV_TABLE['report_year'] = self.DIV_TABLE['report_period'].astype('str').str[:-4]
+        # wind里面有已经实施但是ex_dt字段为空的值
+        # 不用ex_dt作为参照
+        self.DIV_TABLE['ann_date'] = self.DIV_TABLE['ann_date'].astype('int')
+        self.MV_TABLE['ann_date'] = self.MV_TABLE['ann_date'].astype('int')
+
+        # -------------------按照个股迭代------------- #
+        # 筛选出MV表中历史信息 ex_dt
+        for code in tqdm(self.MV_TABLE['stockcode'].unique()):
+            # 选取单个股票
+            df_div = self.DIV_TABLE[self.DIV_TABLE['stockcode'] == code]
+            df_code = self.MV_TABLE[self.MV_TABLE['stockcode'] == code]
+
+            # -------------------按照日期逐步迭代------------- #
+            # 按照当前日期搜索历史信息
+            def search_history(x):
+                # 按照ann_date和ex_dt筛选历史股息信息
+                div_info = df_div[df_div['ann_date'] < x['ann_date']]
+
+                # 按照年份计算筛选后历史的股息
+                div_info_year = div_info.groupby(['report_year']).agg({'dvd_pre_tax': 'sum', 'stockcode': 'count'})
+                div_info_year.reset_index(inplace=True)
+                # if not div_info_year.empty:
+                #     print(div_info_year)
+                # 滞后
+
+                div_info_year = div_info_year[div_info_year['report_year'] == str(int(str(x['ann_date'])[:-4]) - 1)]
+                # if not div_info_year.empty:
+                #     print(div_info_year)
+
+                return div_info_year['dvd_pre_tax'].values * 10000, div_info_year['stockcode'].values
+                # if div_info_year.empty:
+                #     return None, None
+                # else:
+                #     print(div_info_year['dvd_pre_tax'].loc[0], div_info_year['stockcode'].loc[0])
+                #     return div_info_year['dvd_pre_tax'].loc[0], div_info_year['stockcode'].loc[0]
+
+            #
+            df_code[['dvd_l1', 'dvd_count_l1']] = df_code.apply(lambda x: search_history(x), axis=1,
+                                                                result_type='expand')
+            self.DIV_RATE_TABLE = pd.concat([self.DIV_RATE_TABLE, df_code])
+
+        self.DIV_RATE_TABLE.to_csv('history.csv')
+        # print(self.DIV_TABLE['report_year'])
+
+        # 匹配
+        # self.DIV_RATE_TABLE = pd.merge(self.MV_TABLE, self.DIV_YEAR_TABLE, how='left', on=['code', ''])
+        # 去除匹配后时间错误的列
 
 
 if __name__ == '__main__':
     app = CalDiv()
-    app.get_div_by_year()
+    # 计算年度分红
+    # app.get_div_by_year()
+
+    # 合并年度分红到每天
     # app.get_merge_table()
-    # app.get_exp_div()
+
+    # 用合并后的表计算预期
+    app.get_exp_div()
