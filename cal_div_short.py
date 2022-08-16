f5e5c37fa69d97ddf1624e63ccaa867e112518b8
colin <colin-bao@github.com>
2022-08-16 08:42:18 +0800

    feat:重构

--- cal_div_short.py
+++ cal_div_short.py
@@ -1,11 +1,20 @@
+import time
+
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-# ----------------参数区----------------#
-LAG_PERIOD = 5  # 滞后期
-MERGE_COLUMN = ['report_year', 'dvd_pre_tax_sum', 'ann_date_max']  # 需要合并的列名
+# ----------------参数和命名----------------#
+LAG_PERIOD = 4  # 滞后期
 REFER_DATE = 's_div_prelandate'  # ann_date,s_div_prelandate
+
+MERGE_COLUMN = ['report_year', 'dvd_pre_tax_sum', REFER_DATE + '_max']  # 计算出的列的命名
+
+AVG_COLUMN = 'AVG_DIV_{}'.format(LAG_PERIOD-1)  # 分红列平均值
+VAR_COLUMN = 'VAR_DIV_{}'.format(LAG_PERIOD-1)  # 分红列的方差
+PRE_COLUMN = ['DIV_L{}'.format(i) for i in reversed(range(LAG_PERIOD)) if i != 0]  # 用于预测的滞后年
+PRODUCT_COLUMN = ['PRODUCT_{}'.format(i) for i in reversed(range(LAG_PERIOD)) if i != 0]  # 积距
+PRED_COLUMN = 'PRED_DIV_{}'.format(LAG_PERIOD)  # 预测出的分红
 
 # ----------------读取原始数据----------------#
 MV_TABLE = pd.read_parquet('mv.parquet')
@@ -13,8 +22,7 @@ DIV_TABLE = pd.read_parquet('AShareDividend.parquet')
 
 # ----------------筛选计算列----------------#
 DIV_TABLE = DIV_TABLE[DIV_TABLE['s_div_progress'] == '3']  # 只保留3
-DIV_TABLE = DIV_TABLE[
-    ['stockcode', 'report_period', 'ann_date', 's_div_prelandate', 'cash_dvd_per_sh_pre_tax', 's_div_baseshare']]
+DIV_TABLE = DIV_TABLE[['stockcode', 'report_period', REFER_DATE, 'cash_dvd_per_sh_pre_tax', 's_div_baseshare']]
 MV_TABLE = MV_TABLE[['stockcode', 'ann_date', ]]
 
 
@@ -47,29 +55,87 @@ def get_div_by_year():
             # 根据report_year在df_date中获得历史数据
             lag = pd.merge(df_date[MERGE_COLUMN], pd.DataFrame(df_date['report_year'] - lag_t + 1),
                            how='right', on=['report_year'], )
-            lag.rename(columns={k: str(v).format(lag_t) for k, v in RENAME_COLUMN.items()}, inplace=True)  # 列名处理
+            lag.rename(columns={k: str(v).format(lag_t + 1) for k, v in RENAME_COLUMN.items()}, inplace=True)  # 列名处理
             # 合并历史数据
             df_date = pd.concat([df_date, lag], axis=1)
 
         # 拼接每只股票
         DIV_YEAR_TABLE = pd.concat([DIV_YEAR_TABLE, df_date])
 
-    # ----------------保存----------------#
-    DIV_YEAR_TABLE.to_parquet('div_by_year.parquet', index=False)
+    # ----------------合并年度分红表与市值表----------------#
+    # 用上一年的匹配
+    MV_TABLE['report_year'] = (MV_TABLE['ann_date'].astype('str').str[:-4].astype('int')) - 1
+    MERGE_TABLE = pd.merge(MV_TABLE, DIV_YEAR_TABLE, how='left', on=['stockcode', 'report_year'])
+    # 保存合并后的 代码-日期-分红表
+    MERGE_TABLE.rename(columns={i: i + '_l0' for i in RENAME_COLUMN}, inplace=True)  # 重命名列便于区分
+    MERGE_TABLE.to_parquet('merge.parquet', index=False)
 
 
 # 计算预期股息
 def get_exp_div():
-    # 年度股息表
-    DIV_YEAR_TABLE = pd.read_parquet('div_by_year.parquet')
-    # 用上一年的匹配
-    MV_TABLE['report_year'] = (MV_TABLE['ann_date'].astype('str').str[:-4].astype('int')) - 1
-    DIV_RATE_TABLE = pd.merge(MV_TABLE, DIV_YEAR_TABLE, how='left', on=['stockcode', 'report_year'])
-    # 增加静态计算逻辑列
-    # DIV_RATE_TABLE.to_csv('merge.csv', index=False)
-    DIV_RATE_TABLE.to_parquet('merge.parquet', index=False)
-    pass
+    # 用合并后的表计算
+    MERGE_TABLE = pd.read_parquet('merge.parquet')
+
+    # ----------------计算能够使用的历史分红信息----------------#
+    for i in range(LAG_PERIOD):
+        # 使用当前日期与历史发布日期比较大小
+        MERGE_TABLE['INFO_L{}'.format(i)] = np.where(
+            MERGE_TABLE['ann_date'].astype('float') > MERGE_TABLE[REFER_DATE + '_max_l{}'.format(i)].astype('float'), 1,
+            0)
+    # ----------------把历史信息矩阵映射到分红的值----------------#
+    for i in range(LAG_PERIOD):
+        MERGE_TABLE['DIV_L{}'.format(i)] = np.where(
+            MERGE_TABLE['INFO_L{}'.format(i)] == 0, 0, MERGE_TABLE['dvd_pre_tax_sum_l{}'.format(i)])
+
+    # ----------------当前没有T-1期分红信息的处理1:用T-2期分红信息替代----------------#
+    MERGE_TABLE['INFO_L01'] = np.where(
+        MERGE_TABLE['INFO_L0'] == 0, MERGE_TABLE['ann_date'] - MERGE_TABLE[REFER_DATE + '_max_l1'].astype('float'), 1)
+    MERGE_TABLE['INFO_L01'] = np.where(
+        (20000 > MERGE_TABLE['INFO_L01']) & (MERGE_TABLE['INFO_L01'] > 0), 1, 0)  # 只用2年内的分红信息
+    MERGE_TABLE['INFO_L01'].fillna(0, inplace=True)
+    # 在原有的T-1期为0的数据上填充
+    MERGE_TABLE['INFO_L01'] = np.where(
+        MERGE_TABLE['INFO_L0'] == 0, MERGE_TABLE['INFO_L01'], 1)
+    # 映射到T-2期分红的值
+    MERGE_TABLE['DIV_L01'] = np.where(
+        MERGE_TABLE['INFO_L0'] == 0, 0, MERGE_TABLE['dvd_pre_tax_sum_l1'])
+
+    # ----------------当前没有T-1期分红信息的处理2:OLS线性预测----------------#
+    # 计算X的方差 (样本 自由度-1)
+    var_x = np.var(range(LAG_PERIOD), ddof=1)
+    # 计算X的均值
+    avg_x = np.average(range(LAG_PERIOD))
+    # 计算Y的均值
+    MERGE_TABLE[AVG_COLUMN] = np.average(MERGE_TABLE[PRE_COLUMN].astype('float'), axis=1)
+    # 计算Y的方差
+    MERGE_TABLE[VAR_COLUMN] = np.var(MERGE_TABLE[PRE_COLUMN], axis=1)
+
+    # ----------------计算XY的协方差----------------#
+    # 逐个计算出积距: (Yi-Y)*(Xi-X)
+    for i in range(LAG_PERIOD - 1):
+        MERGE_TABLE[PRODUCT_COLUMN[i]] = (MERGE_TABLE[PRE_COLUMN[i]] - MERGE_TABLE[AVG_COLUMN]) * (i - var_x)
+    # 协方差(样本): SUM(Yi-Y)*(Xi-X)/N-1
+    MERGE_TABLE['COV_XY'] = (np.sum(MERGE_TABLE[PRODUCT_COLUMN], axis=1)) / (LAG_PERIOD - 1)
+
+    # ----------------计算Beta_hat----------------#
+    # 斜率: Beta_hat = COV(XY)/COV(XX)
+    MERGE_TABLE['BETA_HAT'] = MERGE_TABLE['COV_XY'] / var_x
+
+    # ----------------计算Alpha_hat---------------#
+    # 截距: Alpha_hat=AVG(Y)-Beta_hat*AVG(X)
+    MERGE_TABLE['ALPHA_HAT'] = MERGE_TABLE[AVG_COLUMN] - MERGE_TABLE['BETA_HAT'] * avg_x
+
+    # ----------------预测Y----------------#
+    # 预测:
+    MERGE_TABLE[PRED_COLUMN] = MERGE_TABLE['ALPHA_HAT'] + MERGE_TABLE['BETA_HAT'] * (LAG_PERIOD-1)
+
+    # ----------------去除预测的负值----------------#
+    MERGE_TABLE[PRED_COLUMN] = np.where(MERGE_TABLE[PRED_COLUMN] < 0, 0, MERGE_TABLE[PRED_COLUMN])
+    # 保存结果
+    MERGE_TABLE.to_csv('cal_div.csv')
 
 
 if __name__ == '__main__':
-    get_div_by_year()  # 生成年度股息表
+    # 生成年度股息表
+    # get_div_by_yeara()
+    get_exp_div()
